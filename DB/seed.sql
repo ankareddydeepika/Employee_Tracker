ff8358457ea732d5b616cd64d3a4d0c5f135653a
Deepika Ankareddi <ankareddydeepika@gmail.com>
2020-05-22 18:08:47 -0400

    added code to add new employee to the employee table

--- DB/seed.sql
+++ DB/seed.sql
@@ -10,8 +10,8 @@ VALUES
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Salesperson', 80000, 1),
-    ('Software Engineer', 120000, 2)
+    ('Salesperson', 60000, 1),
+    ('Software Engineer', 150000, 2)
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
--- employeeTraker.js
+++ employeeTraker.js
@@ -1,6 +1,7 @@
 
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+const util = require("util");
 require("console.table");
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,7 +14,9 @@ var connection = mysql.createConnection({
       if (err) throw err;
       runSearch();
     });
-  function runSearch(){
+   connection.query = util.promisify(connection.query);
+  
+   function runSearch(){
       inquirer.prompt({
         name: "action",
         type: "list",
@@ -37,24 +40,108 @@ var connection = mysql.createConnection({
           case "View roles":
             viewAllroles();
             break;
+
+          case "View departments":
+            viewAlldepartments();
+            break;
+
+          case "Add new employee":
+            addNewEmployee()
+            break;
+          
+          case "Add new department":
+
+          break;
+
         }
       })
   }
 
-  function viewAllemployees(){
-    var query = " SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-    connection.query(query, function(err, res){
-      if(err) throw err;
-      console.table(res);
-      runSearch();
-    })
+  async function viewAllemployees(){
+    var query = allEmployes()
+    var employees = await connection.query(query)
+    console.table(employees);
+    runSearch();
   }
 
-  function viewAllroles(){
-    var query = " SELECT role.id, role.title, department.name, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
-    connection.query(query, function(err, res){
-      if(err) throw err;
-      console.table(res);
+  async function viewAllroles(){
+    var query = allroles()
+    const roles = await connection.query(query)
+    console.table(roles)
+    runSearch()
+  }
+
+  async function viewAlldepartments(){
+    var query = allDepartments()
+    const department = await connection.query(query)
+      console.table(department);
+      runSearch();
+  }
+
+  async function addNewEmployee(){
+  var rolesQuery = allroles()
+   var roles = await connection.query(rolesQuery)
+   var employeesQuery = allEmployes()
+   var employees = await connection.query(employeesQuery)
+  
+   const roleChoices = roles.map(({ id, title }) => ({
+    name: title,
+    value: id
+  }));
+  const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+    name: `${first_name} ${last_name}`,
+    value: id
+  }));
+    await inquirer.prompt([{
+      name:"firstname",
+      type: "input",
+      message: "What is employee's First name?"
+    },
+  {
+    name:"lastname",
+      type: "input",
+      message: "What is employee's Last name?"
+  },
+  {
+  name:"role",
+  type: "list",
+  message: "What is the role of employee?",
+  choices: roleChoices
+
+  },
+  {
+    name:"ManagerDetails",
+    type: "list",
+    message: "Who is the employees Manager",
+    choices: managerChoices
+  
+    }])
+  .then(function(answer){
+    var query = "INSERT INTO employee(first_name, last_name,role_id, manager_id) values( ? , ?, ?, ?);"
+    connection.query(query, [answer.firstname, answer.lastname, answer.role, answer.ManagerDetails], function (err, res) {
+      if (err)throw err;
+      console.log("Added a new employee")
       runSearch();
     })
+  })
   }
+
+  async function addNewdepartment(){
+
+  }
+
+  function allroles() {
+    var query = " SELECT role.id, role.title, department.name, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+    return query
+  }
+
+  function allEmployes() {
+    var query = " SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+    return query
+  }
+
+  function allDepartments(){
+    var query = " SELECT * FROM department;"
+    return query
+  }
+  
